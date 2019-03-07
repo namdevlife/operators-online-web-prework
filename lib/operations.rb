fb17227a42010c6706ab77ec4968ba34c86bd4ab
NAM NGUYEN <namdevlife@gmail.com>
2019-03-07 08:26:41 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,15 @@
 def unsafe?(speed)
-
+  if speed < 40 || speed > 60
+    "unsafe"
+else 
+    "safe"
 end
 
-
-
 def not_safe?(speed)
+  speed < 40 || speed > 60 ? "unsafe" : "safe"
+  
+  
+  
 	
 end
 	
